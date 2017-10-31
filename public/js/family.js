71f2a3bcf22c866f459208737935ad1bd1a6d30a
Cameron Earl <developer.cameron@gmail.com>
2017-10-31 11:47:06 -0700

    Fix forms on various pages add scaffolding for seeds add main.js which isn't being used and probably doesn't need to exist

---
+++ public/js/family.js
@@ -0,0 +1,22 @@
+let addBtnText, icon, addChildForm;
+
+$(document).ready(() => {
+  addBtnText = $('#toggle-add-child-form > span');
+  icon = $('#toggle-add-child-form > i');
+  addChildForm = $('form');
+  $('#toggle-add-child-form').click(toggleForm);
+  addChildForm.hide();
+});
+
+function toggleForm() {
+  if (icon.text() === 'person_add') {
+    icon.text('expand_less');
+    addBtnText.text('Hide');
+    addChildForm.show();
+    addChildForm.find('input[name="first_name"]').focus();
+  } else {
+    icon.text('person_add');
+    addBtnText.text('Add Children');
+    addChildForm.hide();
+  }
+};
