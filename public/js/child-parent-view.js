7c057248ba43a8807c5b213ed264de05ac2f21d6
Cameron Earl <developer.cameron@gmail.com>
2017-11-02 16:02:58 -0700

    Make approve button hidden unless completed

--- public/js/child-parent-view.js
+++ public/js/child-parent-view.js
@@ -1,36 +1,38 @@
+const choreIcons = {
+  "Dust Surfaces": '/images/icons/duster.svg',
+  "Sweep": '/images/icons/sweeper.svg',
+  "Vacuum": '/images/icons/vacuum.svg',
+  "Mop": '/images/icons/mop.svg',
+  "Wash Dishes": '/images/icons/washdishes.svg',
+  "Clean Bedroom": '/images/icons/cleanbedroom.svg',
+  "Put Things in Place": '/images/icons/putthingsinplace.svg',
+  "Clean Windows": '/images/icons/cleanwindows.svg',
+  "Clean Mirrors": '/images/icons/cleanmirrors.svg',
+  "Organize Closet": '/images/icons/organizecloset.svg',
+  "Clean Blinds": '/images/icons/cleanblinds.svg',
+  "Clean Curtains": '/images/icons/cleancurtains.svg',
+  "Clean TV Screens": '/images/icons/cleantv.svg',
+  "Fold Clothes": '/images/icons/foldclothes.svg',
+  "Hang Clothes": '/images/icons/hangclothes.svg',
+  "Load Washer": '/images/icons/loadwasher.svg',
+  "Unload Washer": '/images/icons/unloadwasher.svg',
+  "Set Table": '/images/icons/settable.svg',
+  "Take Trash Out": '/images/icons/trash.svg',
+  "Clean Kitchen": '/images/icons/cleankitchen.svg',
+  "Load Dishwasher": '/images/icons/loaddishwasher.svg',
+  "Unload Dishwasher": '/images/icons/unloaddishwasher.svg',
+  "Walk Pet": '/images/icons/walkpet.svg',
+  "Feed Pet": '/images/icons/feedpet.svg',
+  "Shower Pet": '/images/icons/showerpet.svg',
+  "Water Plants": '/images/icons/waterplants.svg',
+  "Mow the Lawn": '/images/icons/mow.svg',
+  "Pull Weeds": '/images/icons/pullweeds.svg',
+  "Wash Car": '/images/icons/washcar.svg',
+  "Homework": '/images/icons/homework.svg'
+};
+
 const autoCompleteObj = {
-  data: {
-    "Dust Surfaces": '/images/icons/duster.svg',
-    "Sweep": '/images/icons/sweeper.svg',
-    "Vacuum": '/images/icons/vacuum.svg',
-    "Mop": '/images/icons/mop.svg',
-    "Wash Dishes": '/images/icons/washdishes.svg',
-    "Clean Bedroom": '/images/icons/cleanbedroom.svg',
-    "Put Things in Place": '/images/icons/putthingsinplace.svg',
-    "Clean Windows": '/images/icons/cleanwindows.svg',
-    "Clean Mirrors": '/images/icons/cleanmirrors.svg',
-    "Organize Closet": '/images/icons/organizecloset.svg',
-    "Clean Blinds": '/images/icons/cleanblinds.svg',
-    "Clean Curtains": '/images/icons/cleancurtains.svg',
-    "Clean TV Screens": '/images/icons/cleantv.svg',
-    "Fold Clothes": '/images/icons/foldclothes.svg',
-    "Hang Clothes": '/images/icons/hangclothes.svg',
-    "Load Washer": '/images/icons/loadwasher.svg',
-    "Unload Washer": '/images/icons/unloadwasher.svg',
-    "Set Table": '/images/icons/settable.svg',
-    "Take Trash Out": '/images/icons/trash.svg',
-    "Clean Kitchen": '/images/icons/cleankitchen.svg',
-    "Load Dishwasher": '/images/icons/loaddishwasher.svg',
-    "Unload Dishwasher": '/images/icons/unloaddishwasher.svg',
-    "Walk Pet": '/images/icons/walkpet.svg',
-    "Feed Pet": '/images/icons/feedpet.svg',
-    "Shower Pet": '/images/icons/showerpet.svg',
-    "Water Plants": '/images/icons/waterplants.svg',
-    "Mow the Lawn": '/images/icons/mow.svg',
-    "Pull Weeds": '/images/icons/pullweeds.svg',
-    "Wash Car": '/images/icons/washcar.svg',
-    "Homework": '/images/icons/homework.svg'
-  },
+  data: choreIcons,
   limit: 20
 };
 
@@ -49,12 +51,13 @@ $(document).ready(() => {
   $('.modal').modal();
   $('select').material_select();
 
-
   $('.chore-edit').click(prepareChoreEditForm);
 
   $('.chore-delete').click(prepareChoreDeleteForm);
 
   $('.nav-wrapper > ul').prepend('<li><a href="/family" class="waves-effect waves-light btn">Back</a></li>');
+
+  addChoreIcons();
 });
 
 function prepareChoreEditForm(ev) {
@@ -80,4 +83,15 @@ function prepareChoreDeleteForm(ev) {
 
   confirmDeleteBtn.text(`Delete "${choreTitle}"`);
   confirmDeleteBtn.attr('href',`/family/${childId}/chores/delete/${choreId}`);
+}
+
+function addChoreIcons() {
+  let choreTitles = $('td.chore-title');
+  for (let titleElement of choreTitles) {
+    let title = $(titleElement).text();
+    if (choreIcons[title]) {
+      let iconCell = $(titleElement).parent().find('td.chore-icon');
+      iconCell.append(`<img src="${choreIcons[title]}">`);
+    }
+  }
 }
