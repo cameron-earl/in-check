b6414a707f1c407f212e92b31e362414fae4dfde
Jessica Anais Duarte Cabrera <duarte.jezzica@gmail.com>
2017-11-01 17:12:04 -0700

    added values to chores when you edit them

--- public/js/child-parent-view.js
+++ public/js/child-parent-view.js
@@ -8,9 +8,19 @@ $(document).ready(() => {
     close: 'Ok',
     closeOnSelect: false // Close upon selecting a date,
   });
-});
+  $('.modal').modal();
+  $('.chore-edit').click(function(ev){
+    let choreId = $(ev.target).attr('choreid');
+    let row = $(ev.target).parent().parent();
+    let choreTitle =row.find('.chore-title').text();
+    let choreDescription = row.find('.chore-description').text();
+    let choreValue= row.find('.chore-value').text();
+    console.log(choreTitle);
 
-$(document).ready(function(){
-   // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
-   $('.modal').modal();
- });
+    $('#form-edit').attr('action','/family/chores/edit/' + choreId);
+    $('#edit-title').attr('value', choreTitle)
+    $('#edit-description').attr('value', choreDescription)
+    $('#edit-value').attr('value', choreValue)
+
+  })
+});
