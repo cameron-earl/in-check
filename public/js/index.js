d7cf1b4c0ad12398b2a26d218b489739e92e4065
Jessica Anais Duarte Cabrera <duarte.jezzica@gmail.com>
2017-11-03 09:18:42 -0700

    added opacity

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,3 @@
 $(document).ready(() => {
-
-
-$('.nav-wrapper > ul').hide('<li><a href="/logout" class="waves-effect waves-light btn">Log Out</a></li>');
+  $('.nav-wrapper > ul').hide('<li><a href="/logout" class="waves-effect waves-light btn">Log Out</a></li>');
 });
